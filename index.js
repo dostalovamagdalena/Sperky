25433a69de774cc0009d098b9aefcb55c3053e2b
dostalovamagdalena <dostalova.magdalena@gmail.com>
2023-03-05 15:21:13 +0100

    přizpůsobení dropdown nabídky - termíny

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let dropdown = document.querySelector('.dropdown')
-dropdown.addEventListener('click', (e) => {
-  if (dropdown.classList.contains('closed')) {
-    dropdown.classList.remove('closed')
-  } else {
-    dropdown.classList.add('closed')    
-  }
-})
-
-
+//let dropdown = document.querySelector('.dropdown')
+//dropdown.addEventListener('click', (e) => {
+  //if (dropdown.classList.contains('closed')) {
+    //dropdown.classList.remove('closed')
+ // } else {
+   // dropdown.classList.add('closed')    
+  //}
+//})
 let dropdown2 = document.querySelector('.dropdown2')
+
 dropdown2.addEventListener('click', (e) => {
   if (dropdown2.classList.contains('closed')) {
     dropdown2.classList.remove('closed')
@@ -16,3 +15,6 @@ dropdown2.addEventListener('click', (e) => {
     dropdown2.classList.add('closed')    
   }
 })
+
+
+
