c045bb4ff1c69746016336d07ba29ba974e5ad72
dostalovamagdalena <dostalova.magdalena@gmail.com>
2023-03-05 12:37:47 +0100

    vytvořen soubor index.js a vložení dropdown menu

---
+++ index.js
@@ -0,0 +1,18 @@
+let dropdown = document.querySelector('.dropdown')
+dropdown.addEventListener('click', (e) => {
+  if (dropdown.classList.contains('closed')) {
+    dropdown.classList.remove('closed')
+  } else {
+    dropdown.classList.add('closed')    
+  }
+})
+
+
+let dropdown2 = document.querySelector('.dropdown2')
+dropdown2.addEventListener('click', (e) => {
+  if (dropdown2.classList.contains('closed')) {
+    dropdown2.classList.remove('closed')
+  } else {
+    dropdown2.classList.add('closed')    
+  }
+})
